a231170078bdbe8e76580014e85077a09f96d46b
Mattin Guiheneuf <mattin.guiheneuf@orange.fr>
2023-05-06 14:27:41 +0200

    Réorganisation du dossier et fin de la v-1

---
+++ v-1/LecteurVue/lecteurvue.h
@@ -0,0 +1,32 @@
+#ifndef LECTEURVUE_H
+#define LECTEURVUE_H
+
+#include <QMainWindow>
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class lecteurvue; }
+QT_END_NAMESPACE
+
+class lecteurvue : public QMainWindow
+
+{
+    Q_OBJECT
+
+public:
+    lecteurvue(QWidget *parent = nullptr);
+    ~lecteurvue();
+
+private:
+    Ui::lecteurvue *ui;
+
+public slots :
+    void avancer();
+    void reculer();
+    void lancerDiapo();
+    void arreterDiapo();
+    void filtrerImages();
+};
+
+#endif // LECTEURVUE_H
