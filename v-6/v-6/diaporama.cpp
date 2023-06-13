b284bb77827316045d62672b4573c2c4cd062955
Mattin Guiheneuf <mattin.guiheneuf@orange.fr>
2023-06-13 18:28:16 +0200

    V-6

--- v-6/v-6/diaporama.cpp
+++ v-6/v-6/diaporama.cpp
@@ -6,6 +6,15 @@ diaporama::diaporama(QWidget *parent) :
     ui(new Ui::diaporama)
 {
     ui->setupUi(this);
+
+    QSqlQuery query;
+    query.exec("SELECT idDiaporama, ");
+    for(unsigned int i = 0; query.next(); i++)
+    {
+        ui->tbwChoixDiapo->insertRow(i);
+
+
+    }
 }
 
 diaporama::~diaporama()
